c49d07a17af768a099699f1ea06b78e78de020ef
Siddhartha Bharali <siddharthabharaliassam@gmail.com>
2025-04-11 11:40:59 +0530

    k

---
+++ proxy-server.js
@@ -0,0 +1,24 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+exports.handler = async (event, context) => {
+  const proxy = createProxyMiddleware({
+    target: 'https://login.salesforce.com', 
+    changeOrigin: true,
+    secure: false,
+    onProxyRes: (proxyRes, req, res) => {
+      proxyRes.headers['Access-Control-Allow-Origin'] = '*';
+      proxyRes.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS';
+      proxyRes.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization';
+    },
+  });
+
+  return new Promise((resolve, reject) => {
+    proxy(event, context, (err) => {
+      if (err) reject(err);
+      resolve({
+        statusCode: 200,
+        body: 'Proxy request successful',
+      });
+    });
+  });
+};
