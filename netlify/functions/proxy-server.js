041dccd32895f1429223aa3b0599b14198e86fb4
Siddhartha Bharali <siddharthabharaliassam@gmail.com>
2025-04-11 20:04:44 +0530

    another

--- netlify/functions/proxy-server.js
+++ netlify/functions/proxy-server.js
@@ -7,29 +7,38 @@ exports.handler = async function (event) {
   return new Promise((resolve, reject) => {
     const req = https.request(url, {
       method: event.httpMethod,
-      headers: event.headers,
+      headers: {
+        ...event.headers,
+        host: 'login.salesforce.com',
+      },
     }, (res) => {
-      let data = '';
-
-      res.on('data', chunk => data += chunk);
+      let body = '';
+      res.on('data', (chunk) => body += chunk);
       res.on('end', () => {
         resolve({
           statusCode: res.statusCode,
-          body: data,
+          body: body,
           headers: {
-            ...res.headers,
             'Access-Control-Allow-Origin': '*',
             'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
             'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-          }
+            'Content-Type': res.headers['content-type'] || 'application/json'
+          },
         });
       });
     });
 
-    req.on('error', reject);
+    req.on('error', (error) => {
+      reject({
+        statusCode: 500,
+        body: JSON.stringify({ error: error.message })
+      });
+    });
+
     if (event.body) {
       req.write(event.body);
     }
+
     req.end();
   });
 };
