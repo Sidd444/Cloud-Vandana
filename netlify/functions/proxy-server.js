9c3079351f256fa9caa9a4b98abe638a65dd317c
Siddhartha Bharali <siddharthabharaliassam@gmail.com>
2025-04-11 20:54:49 +0530

    ll

--- netlify/functions/proxy-server.js
+++ netlify/functions/proxy-server.js
@@ -1,39 +1,52 @@
-const https = require('https');
+const https = require("https");
 
 exports.handler = async (event) => {
   const targetBase = "https://login.salesforce.com";
-  const path = event.rawUrl.split('/.netlify/functions/proxy-server')[1];
+  const path = event.rawUrl.split("/.netlify/functions/proxy-server")[1];
   const url = targetBase + path;
 
   const headers = { ...event.headers };
-  delete headers.host; 
+  delete headers.host;
 
   return new Promise((resolve, reject) => {
-    const req = https.request(url, {
-      method: event.httpMethod,
-      headers,
-    }, (res) => {
-      let body = '';
+    const req = https.request(
+      url,
+      {
+        method: event.httpMethod,
+        headers,
+      },
+      (res) => {
+        let body = "";
 
-      res.on('data', chunk => body += chunk);
-      res.on('end', () => {
-        resolve({
-          statusCode: res.statusCode,
-          headers: {
-            ...res.headers,
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS, PATCH',
-            'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-          },
-          body,
+        res.on("data", (chunk) => (body += chunk));
+        res.on("end", () => {
+          const sanitizedHeaders = {};
+          for (const key in res.headers) {
+            if (Array.isArray(res.headers[key])) {
+              sanitizedHeaders[key] = res.headers[key].join(",");
+            } else {
+              sanitizedHeaders[key] = res.headers[key];
+            }
+          }
+
+          resolve({
+            statusCode: res.statusCode || 200,
+            headers: {
+              ...sanitizedHeaders,
+              "Access-Control-Allow-Origin": "*",
+              "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS, PATCH",
+              "Access-Control-Allow-Headers": "Content-Type, Authorization",
+            },
+            body,
+          });
         });
-      });
-    });
+      }
+    );
 
-    req.on('error', err => {
+    req.on("error", (err) => {
       reject({
         statusCode: 500,
-        body: JSON.stringify({ message: err.message })
+        body: JSON.stringify({ message: err.message }),
       });
     });
 
