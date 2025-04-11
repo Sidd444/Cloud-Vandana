a9faad994ac07f6b5cef18e3c723988071977f55
Siddhartha Bharali <siddharthabharaliassam@gmail.com>
2025-04-11 20:00:29 +0530

    ll

--- netlify/functions/proxy-server.js
+++ netlify/functions/proxy-server.js
@@ -1,24 +1,35 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const https = require('https');
 
-exports.handler = async (event, context) => {
-  const proxy = createProxyMiddleware({
-    target: 'https://login.salesforce.com', 
-    changeOrigin: true,
-    secure: false,
-    onProxyRes: (proxyRes) => {
-      proxyRes.headers['Access-Control-Allow-Origin'] = '*';
-      proxyRes.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS';
-      proxyRes.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization';
-    },
-  });
+exports.handler = async function (event) {
+  const path = event.path.replace('/.netlify/functions/proxy-server', '');
+  const url = `https://login.salesforce.com${path}`;
 
   return new Promise((resolve, reject) => {
-    proxy(event, context, (err) => {
-      if (err) reject(err);
-      resolve({
-        statusCode: 200,
-        body: 'Proxy request successful',
+    const req = https.request(url, {
+      method: event.httpMethod,
+      headers: event.headers,
+    }, (res) => {
+      let data = '';
+
+      res.on('data', chunk => data += chunk);
+      res.on('end', () => {
+        resolve({
+          statusCode: res.statusCode,
+          body: data,
+          headers: {
+            ...res.headers,
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+            'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+          }
+        });
       });
     });
+
+    req.on('error', reject);
+    if (event.body) {
+      req.write(event.body);
+    }
+    req.end();
   });
 };
